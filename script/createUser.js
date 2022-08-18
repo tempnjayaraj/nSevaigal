f837be33dbdfa2ee92dafef99ec2c0d0a578bb5a
tempnjayaraj <tempnjayaraj@gmail.com>
2022-08-18 13:24:59 +0530

    Util Package

--- script/createUser.js
+++ script/createUser.js
@@ -7,41 +7,6 @@ function changeImages(){
     }
 }
 
-function hide(elements){
-    var ALength = elements.length;
-    for(var i=0;i<ALength;i++){
-        _hide(elements[i]);
-    }
-}
-
-function showBlock(elements){
-    var ALength = elements.length;
-    for(var i=0;i<ALength;i++){
-        show(elements[i],false);
-    }
-}
-
-function showInline(elements){
-    var ALength = elements.length;
-    for(var i=0;i<ALength;i++){
-        show(elements[i],true);
-    }
-}
-
-function _hide(element){
-    document.getElementById(element).style.visibility = 'hidden';
-    document.getElementById(element).style.display = 'none';
-}
-
-function show(element,boolean){
-    document.getElementById(element).style.visibility = 'visible';
-    if(boolean==true){
-        document.getElementById(element).style.display = 'inline';
-    }else{
-        document.getElementById(element).style.display = 'block';
-    }    
-}
-
 function changeOccu(num){
     const occupation = document.getElementById('occu'+num).value;
     if(occupation==""){
---
+++ script/util.js
@@ -0,0 +1,34 @@
+function hide(elements){
+    var ALength = elements.length;
+    for(var i=0;i<ALength;i++){
+        _hide(elements[i]);
+    }
+}
+
+function showBlock(elements){
+    var ALength = elements.length;
+    for(var i=0;i<ALength;i++){
+        show(elements[i],false);
+    }
+}
+
+function showInline(elements){
+    var ALength = elements.length;
+    for(var i=0;i<ALength;i++){
+        show(elements[i],true);
+    }
+}
+
+function _hide(element){
+    document.getElementById(element).style.visibility = 'hidden';
+    document.getElementById(element).style.display = 'none';
+}
+
+function show(element,boolean){
+    document.getElementById(element).style.visibility = 'visible';
+    if(boolean==true){
+        document.getElementById(element).style.display = 'inline';
+    }else{
+        document.getElementById(element).style.display = 'block';
+    }    
+}
